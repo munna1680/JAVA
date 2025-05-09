289db8d43a5c5d4457123efb199ca59d4313de7c
Munna <shakibhassanmunna777@gmail.com>
2025-05-09 23:01:31 +0600

    practice

--- Basic_to_Pro/src/main/java/Basic_to_Pro/Assignment1.java
+++ Basic_to_Pro/src/main/java/Basic_to_Pro/Assignment1.java
@@ -1,7 +1,9 @@
 package Basic_to_Pro;
 
-public class Assignment1 {
-    public static void main(String[] args){
+public class Assignment1
+{
+    public static void main(String[] args)
+    {
         System.out.println(" id: 101,");
         System.out.println("title: iphone15,");
         System.out.println("price: 1895 euros,");
---
+++ Basic_to_Pro/src/main/java/Basic_to_Pro/Assignment2.java
@@ -0,0 +1,18 @@
+package Basic_to_Pro;
+
+public class Assignment2
+{
+    public static void main(String[] args)
+    {
+        int id = 101;
+        String title = "iphone15";
+        double price = 1895.00;
+        String description = "perfect product with best image quality";
+        String category = "phone";
+
+        System.out.println("id: " + id + ",");
+        System.out.println("title: " + title + ",");
+        System.out.println("price: " + price + ",");
+        System.out.println("description: " + description + ",");
+    }
+}
