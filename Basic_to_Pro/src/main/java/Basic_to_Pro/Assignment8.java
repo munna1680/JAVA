10f839a45dc2a973445a7b91f2f271bde50b7b02
Munna <shakibhassanmunna777@gmail.com>
2025-05-16 00:27:43 +0600

    java

---
+++ Basic_to_Pro/src/main/java/Basic_to_Pro/Assignment8.java
@@ -0,0 +1,14 @@
+package Basic_to_Pro;
+
+public class Assignment8
+{
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        for(int i = 1; i <= 10;i++)
+        {
+            sum += i;
+        }
+        System.out.println("Sum of 10 numbers: " + sum);
+    }
+}
