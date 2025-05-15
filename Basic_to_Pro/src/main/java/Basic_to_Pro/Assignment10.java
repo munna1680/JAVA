0dfcfae248ea2774cd32c884627caaa23d9606d6
Munna <shakibhassanmunna777@gmail.com>
2025-05-16 01:05:35 +0600

    java

---
+++ Basic_to_Pro/src/main/java/Basic_to_Pro/Assignment10.java
@@ -0,0 +1,25 @@
+package Basic_to_Pro;
+
+import java.util.Scanner;
+
+public class Assignment10
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int sum = 0;
+        for(int i = num1; i <= num2;i++)
+        {
+            if(i % 2 != 0)
+            {
+                sum += i;
+            }
+        }
+        System.out.println("Sum of odd numbers between " + num1 + " and " + num2 + " is " + sum);
+
+    }
+}
