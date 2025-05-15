0047631dfc253149d58111d41a53feeeded82476
Munna <shakibhassanmunna777@gmail.com>
2025-05-16 00:49:49 +0600

    java

---
+++ Basic_to_Pro/src/main/java/Basic_to_Pro/Assignment9.java
@@ -0,0 +1,18 @@
+package Basic_to_Pro;
+import java.util.Scanner;
+
+public class Assignment9
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num1 = sc.nextInt();
+        int fact = 1;
+        for(int i = 1; i <= num1; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + num1 + " is " + fact);
+    }
+}
